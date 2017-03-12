06bb1d11914dadb5363a769975139dfb206f51f0
Tashiqi <redouane.elghazi@gmail.com>
2017-03-12 20:27:02 +0100

    minor text fix

--- WorldGenerator/createWorld.cpp
+++ WorldGenerator/createWorld.cpp
@@ -1,6 +1,6 @@
 #include "createWorld.h"
 
-int compute_proba(double length) {
+int compute_proba(size_t length) {
 	if(length < c_length_min) return 0;
 	else return length-c_length_min;
 }
@@ -9,24 +9,25 @@ int reverse_proba() {
 
 }
 
-void update_world(int ***world, const Cuboid &cuboid)
+void update_world(std::vector<std::vector<std::vector<int> > >& world, const Cuboid &cuboid)
 {
 
 }
 
-void next_cuboid(int ***world, vector<Cuboid> &world);
+void next_cuboid(std::vector<std::vector<std::vector<int> > >& world, std::vector<Cuboid> &cuboid)
 {
 	std::map<std::pair<int, int>, double> probas;
 
-	double theta = c_theta_0, phi = c_theta_0; 
-	for(int i = -nb_angles; i < nb_angles) { // i = theta
-		for(int j = -nb_angles; j < nb_angles) { // j = phi
-			type e_x, e_y, e_z;
-			for(size_t length = 0; length < c_length_max; ++length) {
-				for(int k = -height; k < height; ++k) {
-					for(int l = -height; l < height; ++l) {
-						int currPos = length*e_z + k * e_y + l * e_x;
-						if(world[currPos.x][currPos.y][currPos.z]  != 0) {
+	double theta = c_theta_0, phi = c_theta_0;
+	for(size_t i = -nb_angles; i < nb_angles; ++i) { // i = theta
+		for(size_t j = -nb_angles; j < nb_angles; ++j) { // j = phi
+			vecteur e_x, e_y, e_z;
+			size_t length;
+			for(length = 0; length < c_length_max; ++length) {
+				for(size_t k = -c_height; k < c_height; ++k) {
+					for(size_t l = -c_height; l < c_height; ++l) {
+						vecteur currPos = length*e_z + k * e_y + l * e_x;
+						if(world[currPos.x()][currPos.y()][currPos.z()]  != 0) {
 							goto end_browse;
 						}
 					}
@@ -42,11 +43,11 @@ void next_cuboid(int ***world, vector<Cuboid> &world);
 }
 
 void createWorld() {
-	int world[c_world_size][c_world_size][c_world_size] = {};
+	std::vector<std::vector<std::vector<int> > > world(c_world_size, std::vector<std::vector<int> > (c_world_size, std::vector<int> (c_world_size, 0)));
 	std::vector<Cuboid> cuboids;
 
 	/// Create first cuboid
-	world.push_back(Cuboid(c_max_length, c_height, 0, 0, Point(0,0,0)));
+	cuboids.push_back(Cuboid(c_length_max, c_height, 0, 0, 0, Point(0,0,0)));
 
 	size_t n = 10;
 	for(size_t i = 0; i < n; ++i) {
--- WorldGenerator/createWorld.h
+++ WorldGenerator/createWorld.h
@@ -1,19 +1,23 @@
-#include "function.h"
+#include <map>
+#include <vector>
+#include "functions.h"
+#define Point point
+#define Vecteur vecteur
 
-const double world_size = 400;
+const size_t c_world_size = 400;
 
-const double c_height = 10, c_length_min = 40, c_length_max = 80, c_phi_max = 90, c_theta_max = 90;
+const size_t c_height = 10, c_length_min = 40, c_length_max = 80, c_phi_max = 90, c_theta_max = 90;
 const size_t nb_angles = 20;
 double c_theta_0 = 90./nb_angles;
 
 class Cuboid
 {
 public:
-	Cuboid(double l, double h, double t, double ph, double ps, point i) { length(l), height(h), theta(th), phi(ph), psi(ps), in(i)
-		
+	Cuboid(double l, double h, double th, double ph, double ps, Point i) : length(l), height(h), theta(th), phi(ph), psi(ps), in(i) {
+
 	}
 
-//private: 
+//private:
 	point in, out;
 	double theta, phi, psi;
 	double length, height;
