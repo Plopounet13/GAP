127c558ccbed9fa977ecdc33981fabba2f0eebf2
NicolasDerumigny <nderumigny@gmail.com>
2017-04-12 12:18:45 +0200

    Updating to UE 4.15

--- GameEngine/Source/GAP/GAP.Build.cs
+++ GameEngine/Source/GAP/GAP.Build.cs
@@ -6,7 +6,7 @@ public class GAP : ModuleRules
 {
 	public GAP(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "PerfCounters" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
